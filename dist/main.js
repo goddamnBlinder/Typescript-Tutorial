e702f2be000f028849ccd6d11564bf89d3cdfe71
James Chinweike <chinweikechukwudi@gmail.com>
2023-03-03 14:52:40 +0100

    t445

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,22 @@
-
+"use strict";
 console.log("Login");
+//? Inheritance 
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+    //*Adding Methods
+    describe() {
+        console.log(`department is ${this.name}`);
+    }
+}
+let department = new Department('accounting');
+department.describe();
+// console.log(department);
+let departmentCopy = {
+    name: "Leela",
+    describe: department.describe
+};
+console.log(departmentCopy);
+departmentCopy.describe();
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
 console.log("Login");
 //? Inheritance 
-Class 
+class Department { 
+    name: string;
+    
+  constructor(name:string){
+    this.name = name;
+  }
+//*Adding Methods
+describe(this:Department){
+    console.log(`department is ${this.name}`);
+    
+}
+
+}
+let department = new Department('accounting');
+department.describe()
+// console.log(department);
+
+let departmentCopy = {
+    name: "Leela",
+    describe: department.describe
+};
+console.log(departmentCopy);
+departmentCopy.describe();
