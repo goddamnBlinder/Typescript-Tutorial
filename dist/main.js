cfd6c199506e6b49310a83066bbd04de62680502
James Chinweike <chinweikechukwudi@gmail.com>
2023-03-03 18:08:43 +0100

    commit

--- dist/main.js
+++ dist/main.js
@@ -2,21 +2,38 @@
 console.log("Login");
 //? Inheritance 
 class Department {
-    constructor(name) {
+    constructor(name, id, employees = []) {
         this.name = name;
+        this.id = id;
+        this.employees = [];
+        this.name = name;
+        this.employees = employees;
+        this.id = id;
     }
     //*Adding Methods
+    addEmployees(employees) {
+        //?Validations
+        this.employees.push(employees);
+    }
+    printEmployees() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
     describe() {
-        console.log(`department is ${this.name}`);
+        console.log(`department is ${this.name} : ${this.id}`);
+        console.log(this.employees);
     }
 }
-let department = new Department('accounting');
-department.describe();
-// console.log(department);
+let department = new Department('accounting', 'd1');
+console.log(department);
+department.addEmployees('leela');
+department.addEmployees('leela');
+department.printEmployees();
+// department.employees [1] = 'Bruno';
+// //direct way to add
 let departmentCopy = {
-    name: "Leela",
     describe: department.describe
 };
 console.log(departmentCopy);
-departmentCopy.describe();
+departmentCopy.describe.bind(department)();
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,25 +1,50 @@
 console.log("Login");
 //? Inheritance 
 class Department { 
-    name: string;
+
+  private  employees: string[] = [];
     
-  constructor(name:string){
+  constructor(private name:string, private id: string, employees:string[] = [] ){
     this.name = name;
+    this.employees = employees;
+    this.id = id;
   }
 //*Adding Methods
+addEmployees(employees:string){
+    //?Validations
+    this.employees.push(employees)
+}
+printEmployees(){
+    console.log(this.employees.length);
+    console.log(this.employees);
+    
+    
+}
+
 describe(this:Department){
-    console.log(`department is ${this.name}`);
+    console.log(`department is ${this.name} : ${this.id}`);
+    console.log(this.employees);   
     
 }
 
 }
-let department = new Department('accounting');
-department.describe()
-// console.log(department);
+
+let department = new Department('accounting', 'd1');
+console.log(department);
+
+
+
+department.addEmployees('leela');
+department.addEmployees('leela');
+
+department.printEmployees();
+
+// department.employees [1] = 'Bruno';
+// //direct way to add
+
 
 let departmentCopy = {
-    name: "Leela",
     describe: department.describe
 };
 console.log(departmentCopy);
-departmentCopy.describe();
+departmentCopy.describe.bind(department)();
